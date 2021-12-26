b263629b50c0911ac456ac066967075716882762
Cason Adams <casonadams@gmail.com>
2021-12-26 15:51:07 -0700

    making which key maps silent

--- lua/_whichkey.lua
+++ lua/_whichkey.lua
@@ -128,7 +128,7 @@ local which_key = {
 }
 
 function map(mode, lhs, rhs, opts)
-	local options = { noremap = true }
+	local options = { noremap = true, silent = true }
 	if opts then
 		options = vim.tbl_extend("force", options, opts)
 	end
